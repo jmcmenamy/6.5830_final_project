abd1deda9837895705caab38fc7bf85ed25f2428
Josiah McMenamy <josiah.mcmenamy@gmail.com>
2024-10-29 02:42:15 -0400

    start thinking about diff parsing strategies

--- setup_database.py
+++ setup_database.py
@@ -1,6 +1,8 @@
 import csv
 import sqlite3
 import os
+import time
+from itertools import islice
 
 DB_FILE = "tpch.db"
 DIR_PATH = f"{os.path.dirname(os.path.abspath(__file__))}"
@@ -34,29 +36,85 @@ def get_table_columns(table_name: str, db_conn) -> list[str]:
     cursor.execute(f"SELECT * FROM {table_name} LIMIT 1")
     return [description[0] for description in cursor.description]
 
+def prepend_generator(val, generator):
+    yield val
+    yield from generator
 
-def load_tables(raw_data_dir: str, db_conn):
+
+def load_tables(raw_data_dir: str, db_conn, parsing_strategy, strategy_name):
     """
     Load raw data from .tbl files in `raw_data_dir` to the database at
     `db_conn`.
     """
     cursor = db_conn.cursor()
+    print(f"loading tables")
+    start_time = time.time()
 
     for table_name in get_table_names(db_conn):
         raw_data_file = f"{raw_data_dir}/{table_name.lower()}.tbl"
         table_columns = get_table_columns(table_name, db_conn)
-        with open(raw_data_file, mode="r") as file:
-            csv_reader = csv.reader(file, delimiter="|")
-            for row in csv_reader:
-                cursor.execute(
-                    f"""
-                    INSERT INTO {table_name} ({", ".join(table_columns)})
-                    VALUES ({", ".join(["?"]*len(table_columns))})
-                    """,
-                    ([field for field in row]),
-                )
 
-            db_conn.commit()
+        with open(raw_data_file, mode="r") as file:
+            strategy_start_time = time.time()
+            parsing_strategy(csv.reader(file, delimiter="|"), cursor, table_name, table_columns)
+            strategy_end_time = time.time()
+            print(f"{strategy_name} on {table_name} done time elapsed: {strategy_end_time - strategy_start_time:.2f}")
+
+        db_conn.commit()
+
+    end_time = time.time()
+    print(f"done loading tables time elapsed={end_time-start_time:.2f} seconds")
+
+def baseline_strategy(csv_reader, chunksize, cursor, table_name, table_columns):
+    """
+    The baseline strategy just sequentially iterates through the whole file
+    """
+    start_time = time.time()
+    while True:
+        chunk = islice(csv_reader, chunksize)
+
+        # this lets us know when the iteration through the csv_reader should end
+        try:
+            chunk = prepend_generator(next(chunk), chunk)
+        except StopIteration:
+            break
+
+        cursor.executemany(
+            f"""
+            INSERT INTO {table_name} ({", ".join(table_columns)})
+            VALUES ({", ".join(["?"]*len(table_columns))})
+            """,
+            chunk
+        )
+
+def sequential_subset(csv_reader, chunksize, cursor, table_name, table_columns, offset, stopping_strategy):
+    """
+    The sequential subset strategy is the same as baseline, but reads only a subset of the data,
+    defined by offset and stopping_strategy
+
+    stopping_strategy can be used to implement a fixed size random sequential parsing, or something more
+    sophisticated (e.g. stopping until a delta change of some aggregate metric is less than some epsilon)
+    """
+    pass
+
+def random_subset(csv_reader, chunksize, cursor, table_name, table_columns, stopping_strategy):
+    """
+    The random subset strategy seeks to a random offset in the file, then parses in both directions,
+    stopping according to the stopping_strategy, which takes in a row and returns whether the parsing
+    should stop or not
+
+    stopping_strategy can be used to implement a fixed size random sequential parsing, or something more
+    sophisticated (e.g. stopping until a delta change of some aggregate metric is less than some epsilon)
+    """
+    pass
+
+def parallel_subsets(csv_reader, chunksize, cursor, table_name, table_columns, num_parallel, stopping_strategy):
+    """
+    The parallel subset strategy starts with num_parallel evenly spaced offets in the file, and parses
+    in both directions from all of them, alternating through each one, stopping according to the stopping_strategy,
+    which takes in a row and returns whether the parsing should stop or not
+    """
+    pass
 
 
 def execute_query(sql_query: str, db_conn):
@@ -73,7 +131,7 @@ if __name__ == "__main__":
     db_conn = sqlite3.connect(DB_FILE)
 
     create_tables(f"{DIR_PATH}/tpch_raw_data/schema.sql", db_conn)
-    load_tables(f"{DIR_PATH}/tpch_raw_data", db_conn)
+    load_tables(f"{DIR_PATH}/tpch_raw_data", db_conn, lambda file, cursor, table_name, table_columns: baseline_strategy(file, 1000, cursor, table_name, table_columns), "baseline_strategy")
 
     # Execute simple query to test data has been successfully loaded.
     # For original (untransformed) data, output should be [(10000,)].
